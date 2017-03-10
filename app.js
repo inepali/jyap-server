b13acea7f9774f136955b55d614c5df7dac25a68
iNepali <sanjayaghimire@gmail.com>
2017-03-10 14:43:39 -0600

    startover

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 var express = require('express');
 var app = express();
 
-var server = http.createServer(app)
+var server = require('http').createServer(app);
 //var io = require('../..')(server);
-var io = require('socket.io').listen(server);
+var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
 // io.configure(function(){
