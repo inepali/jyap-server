ec44423d16ac3c7b02a3f0c26eb9092ddaff940c
iNepali <sanjayaghimire@gmail.com>
2017-03-10 13:58:19 -0600

    io.configure removed

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ var server = http.createServer(app)
 var io = require('socket.io').listen(server);
 var port = process.env.PORT || 8088;
 
-io.configure(function() {
-    io.set('transports', ['websocket']);
-  });
-
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
