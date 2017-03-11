57ba08241fe9caceb37d281ed9569b882ebb935b
iNepali <sanjayaghimire@gmail.com>
2017-03-10 20:32:22 -0600

    config changes ujwal B

--- app1.js
+++ app1.js
@@ -1,13 +1,19 @@
 // Setup basic express server
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
-var io = require('../..')(server);
-var port = process.env.PORT || 3000;
 
-server.listen(port, function () {
-  console.log('Server listening at port %d', port);
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+server.listen(3000, function(){
+  console.log('Server listening');
 });
+//var io = require('../..')(server);
+
+// var port = process.env.PORT || 3000;
+
+// server.listen(port, function () {
+//   console.log('Server listening at port %d', port);
+// });
 
 // Routing
 app.use(express.static(__dirname + '/public'));
