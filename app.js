bfbd9271c18bd3c4d926d60ebd23eeb6cd58569e
iNepali <sanjayaghimire@gmail.com>
2017-03-19 22:29:09 -0500

    Additioanl changes

--- app.js
+++ app.js
@@ -5,16 +5,25 @@ var io = require('socket.io').listen(server);
 var port = process.env.PORT || 3200;
 
 
-players = [];
-deck = [];
-rooms = [];
+var players = [];
+var deck = [];
+var rooms = [];
 
-connections = [];
+var cardTyes = [
+    { key: 'C', name: 'Club' },
+    { key: 'D', name: "Diamond" },
+    { key: 'H', name: "Heart" },
+    { key: 'S', name: "Spade" }];
+var numberOfDeck = 2;
+var _choiceCard;
+
+var connections = [];
 var roomNum = 1;
 var roomSize = 8
 
 server.listen(port, function () {
-    console.log('Server listening at port %d', port);
+    log('Server listening at port ' + port);
+    log(players.length);
 });
 
 app.use(express.static(__dirname + '/public'));
@@ -23,10 +32,71 @@ var log = function (obj) {
     console.log(JSON.stringify(obj));
 };
 
+var prepareDeckAndShuffle = function () {
+
+    // load number of decks
+    deck = [];
+
+    for (var n = 0; n < numberOfDeck; n++) {
+        for (var i = 0; i < cardTyes.length; i++) {
+            for (var j = 1; j <= 13; j++) {
+                var card = {};
+                card.params = { name: cardTyes[i].key + j, num: j, cardType: cardTyes[i].key }
+                deck.push(card);
+            }
+        }
+    }
+
+    //shuffle deck
+    var currentIndex = deck.length, tempValue, randomIndex
+
+    while (0 !== currentIndex) {
+        // pick a remaining element
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // and swap it with the current element
+        tempValue = deck[currentIndex];
+        deck[currentIndex] = deck[randomIndex];
+        deck[randomIndex] = tempValue;
+    }
+    log('Deck Prepared');
+};
+
+var dealGame = function () {
+
+    io.sockets.emit('GAME_ON_OFF', { gameOn: true });
+
+    for (var i = 0; i < 5; i++) {
+        players.forEach(function (player) {
+            //get the top card
+            var card = deck[0];
+
+            if (player.name != null) {
+                player.hands = player.hands || [];
+                player.hands.push(card);
+
+                deck.splice(0, 1);
+            }
+        });
+
+        //chice card 
+        _choiceCard = deck[0];
+        deck.splice(0, 1);
+    }
+
+    io.sockets.emit('UPDATE_PLAYERS', { players: players, choiceCard: _choiceCard });
+    //io.sockets.emit('CHOICE_CARD_SELECTED', { choiceCard: _choiceCard });
+
+    log('Game Deal Completed');
+};
+
 io.sockets.on('connection', function (socket) {
     connections.push(socket.id);
 
     log('Connection started ' + socket.id + ', # of connections ' + connections.length);
+
+    log(players.length);
 
     // console.log(socket.adapter.rooms["room-" + roomNum]);
 
@@ -47,7 +117,7 @@ io.sockets.on('connection', function (socket) {
             }
         }
 
-        log(connections);
+        //log(connections);
         for (var i = 0; i < connections.length; i++) {
             if (connections[i] == socket.id) {
                 connections.splice(i, 1);
@@ -56,7 +126,6 @@ io.sockets.on('connection', function (socket) {
 
 
         io.sockets.emit('PLAYER_LEFT', { player: playerLeft });
-
 
         log('Connection lost ' + socket.id + ', # of connections ' + connections.length);
 
@@ -77,8 +146,21 @@ io.sockets.on('connection', function (socket) {
     socket.on('PLAYER_JOINED', function (data) {
         log(data);
         data.id = socket.id;
+
         players.push(data);
+
+        log(players.length);
+
+        if (players.length == 1) {
+            prepareDeckAndShuffle();
+        }
+
+        if (players.length == 2) {
+            setTimeout(function(){ dealGame(); }, 3000);
+        }
+
         io.sockets.emit('UPDATE_PLAYERS', { players: players, thisPlayer: data });
+        
     });
 
     // when player hit the exit button
@@ -111,6 +193,14 @@ io.sockets.on('connection', function (socket) {
 
         //io.sockets.emit('UPDATE_USERS', {onlineUsers: users});
         showLog();
+    });
+
+    // when player take a card 
+    socket.on("PREPARE_DECK", function (data) {
+        var card = deck[0];
+        deck.splice(0, 1);
+
+        io.sockets.emit('UPDATE_USERS', { onlineUsers: users });
     });
 
     // when player take a card 
